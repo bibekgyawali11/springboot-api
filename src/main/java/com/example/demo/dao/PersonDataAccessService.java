ba2535f430365feefd1e85f0566c34caf832bbb8
Bibek Gyawali <bibekgyawali123@gmail.com>
2020-12-16 14:20:57 -0600

    First Commit

---
+++ src/main/java/com/example/demo/dao/PersonDataAccessService.java
@@ -0,0 +1,59 @@
+package com.example.demo.dao;
+
+import com.example.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository("postgres")
+public class PersonDataAccessService implements PersonDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDataAccessService(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        return 0;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        final String sql = "SELECT id, name FROM person";
+        List<Person> people = jdbcTemplate.query(sql, (resultset, i)-> {
+            UUID id = UUID.fromString(resultset.getString("id"));
+            String name = resultset.getString("name");
+            return new Person(id, name);
+        });
+        return people;
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        final String sql = "SELECT id, name FROM person WHERE id = ?";
+
+        Person person = jdbcTemplate.queryForObject(sql, new Object[]{"id"}, (resultset, i)-> {
+            UUID personId = UUID.fromString(resultset.getString("id"));
+            String name = resultset.getString("name");
+            return new Person(personId, name);
+        });
+        return Optional.ofNullable(person);
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        return 0;
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person person) {
+        return 0;
+    }
+}
